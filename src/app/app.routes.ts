cfa4b1b5df48f150cf9ca9a25cbf0ae78d1064f7
nuriev <ramaketa@gmail.com>
2024-07-07 13:22:44 +0300

    Добавление палетки Добавление и настройка шрифтов Добавление страницы 404

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { PageNotFoundComponent } from './components/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const APP_ROUTES: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'chat'
+    },
+    {
+        path: 'chat',
+        loadChildren: () => import('./components/chat/chat.routes').then(m => m.CHAT_ROUTES)
+    },
+    {
+        path: '**',
+        component: PageNotFoundComponent
+    }
+];
---
+++ src/app/components/chat/chat.routes.ts
@@ -0,0 +1,7 @@
+import { Routes } from '@angular/router';
+import { ChatComponent } from './chat.component';
+
+export const CHAT_ROUTES: Routes = [{
+    path: '',
+    component: ChatComponent
+}];
